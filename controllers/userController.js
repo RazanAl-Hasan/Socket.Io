508010c0fba7e47843513af56acb984af932f43e
RazanAlhasan <razanalhasan51@gmail.com>
2025-07-18 18:05:42 +0300

    Add user model and controller for chat logi

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+
+exports.newUser = async (socket, io, username) => {
+    const user = new User({ username, socketId: socket.id });
+    await user.save();
+    socket.broadcast.emit('user-connected', username);
+};
+
+exports.sendMessage = async (socket, io, message) => {
+    const user = await User.findOne({ socketId: socket.id });
+    if (user) {
+      socket.broadcast.emit('chat-message', {
+        username: user.username,
+        text: message
+      });
+    }
+  };
+
+exports.disconnect = async (socket, io) => {
+  try {
+    const user = await User.findOneAndDelete({ socketId: socket.id });
+    if (user) {
+      socket.broadcast.emit('user-disconnected', user.username);
+    }
+  } catch (err) {
+    console.error('❌ Error in disconnect:', err);
+  }
+};
